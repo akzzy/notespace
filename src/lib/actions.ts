37b64aa5877ced8b5a16a72da5fd91846b1bc579
akzzy forza <akzzy.forza@gmail.com>
2025-09-25 08:39:10 +0000

    set a maximum character size for the notes. i dont want users dumping mi

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -5,7 +5,7 @@ import * as db from './db';
 import { z } from 'zod';
 
 const NoteSchema = z.object({
-  content: z.string().min(1).max(2000),
+  content: z.string().min(1).max(1000),
   userId: z.string(),
 });
 
